9ecbe7ac8d6b321a4fdd0294e0ebfa820e091dd3
Deymeson <deymeson57@gmail.com>
2026-08-30 15:11:03 -0300

    Add native MUGEM character packages

---
+++ characters/hero_naruto_first/zpng.sh
@@ -0,0 +1,20 @@
+def_name="hero_naruto_first"
+
+rm -rf $def_name*
+for file in `ls *png`
+do
+mv $file $def_name"0"$file
+done
+for file in `ls *json`
+do
+file_no=`echo $file | cut -b1-1`
+file_nm=`echo $file_no".png"`
+package_name=`echo $def_name"0"$file_no".png"`
+awk '{ gsub(/'$file_nm'/,"'$package_name'"); print $0 }' $file > $def_name$file
+rm $file
+done
+mv "../"$def_name".json" "./"$def_name".json"
+for file in `ls *mp3`
+do
+mv $file $def_name"_"$file
+done
